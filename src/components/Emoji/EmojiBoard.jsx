61c079feb12cbf9c43c1344c36ab2f2a25b61b1d
jbuyu <jbuyu@ymail.com>
2020-07-30 16:07:28 +0300

    styled components for emoji and button, adds list of emojis

--- src/components/Emoji/EmojiBoard.jsx
+++ src/components/Emoji/EmojiBoard.jsx
@@ -1,7 +1,37 @@
 import React, { Component } from "react";
 import Emoji from "./Emoji";
+import EmojiButton from "./EmojiButton";
 const EmojiBoard = () => {
-  return <Emoji label="Thumbs Up" symbol="👍" size="3rem" />;
+  const emojis = [
+    {
+      label: "Thumbs Up",
+      symbol: "👍",
+    },
+    {
+      label: "Mind Blown",
+      symbol: "🤕",
+    },
+    {
+      label: "Heart eyes",
+      symbol: "💌",
+    },
+    {
+      label: "Dog",
+      symbol: "🐶",
+    },
+    {
+      label: "shyt",
+      symbol: "💩",
+    },
+  ];
+  return (
+    <EmojiButton
+      onClick={() => handleEmojiClick()}
+      onkeydown={(e) => handleEmojiKeyDown(e)}
+    >
+      <Emoji label="Thumbs Up" symbol="👍" size="3rem" />;
+    </EmojiButton>
+  );
 };
 
 export default EmojiBoard;
